23c1a9d93be09ac33fcd7e31afe7b8f6219c463a
wk <somnuk.wk@gmail.com>
2022-07-01 03:02:49 +0700

    Httpie

--- src/MyWeb/Program.cs
+++ src/MyWeb/Program.cs
@@ -3,38 +3,27 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<FormOptions>(x =>
-    {
-        // x.MultipartBodyLengthLimit = 1048576;
-        var max = 1024 * 1024 * 30;
-        x.MultipartBodyLengthLimit = max;
-        Console.WriteLine(max);
-    });
+{
+    var max = 1024 * 1024 * 30;
+    x.MultipartBodyLengthLimit = max;
+});
 
 builder.Services.Configure<KestrelServerOptions>(options =>
 {
-    options.Limits.MaxRequestBodySize = int.MaxValue; // if don't set default value is: 30 MB
+    options.Limits.MaxRequestBodySize = int.MaxValue;
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
